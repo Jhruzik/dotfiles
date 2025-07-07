2659b648ed39ab62f6a119a750a730098d7dee21
Joshua Hruzik <joshua.hruzik@protonmail.com>
2025-07-07 22:40:35 +0200

    Install Sway Desktop for Fedora

--- install_desktop.sh
+++ install_desktop.sh
@@ -1,8 +1,2 @@
-# Install Dependencies
-sudo apt install xorg lightdm lightdm-gtk-greeter \
-		i3-wm i3lock i3status i3blocks dmenu \
-		terminator lxappearance fonts-font-awesome fonts-hack-ttf
-
-# Activate Desktop Manager
-sudo systemctl enable lightdm.service
-sudo systemctl start lightdm.service
+# Install Sway + Waybar + Terminal Emulator
+sudo dnf install sway waybar alacritty
